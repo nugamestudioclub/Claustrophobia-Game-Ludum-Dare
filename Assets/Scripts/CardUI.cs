9d8b98acc77e7814eec218b3b5ab4334ba44430a
MarcoHampel <sebastianhampel1@gmail.com>
2023-09-30 15:42:20 -0400

    Post Processing Changes

--- Assets/Scripts/CardUI.cs
+++ Assets/Scripts/CardUI.cs
@@ -9,6 +9,10 @@ public class Card : MonoBehaviour
 
     [SerializeField]
     GameObject cardPrefab;
+    [SerializeField]
+    private float xOffset;
+    [SerializeField]
+    private float yOffset;
 
     List<int> cardPosX = new List<int> { -200, -100, 0, 100, 200 };
     List<int> cardPosY = new List<int> { 170, 200, 210, 200, 170 };
@@ -33,8 +37,9 @@ public class Card : MonoBehaviour
         for (var i = 0; i < 5; i++)
         {
             GameObject card = Instantiate(cardPrefab, canvas.transform);
-            card.transform.localPosition = new Vector3(cardPosX[i], cardPosY[i], 0);
-            card.transform.position = new Vector3(card.transform.position.x, cardPosY[i], card.transform.position.z);
+            card.transform.localPosition = new Vector3(cardPosX[i]+xOffset, cardPosY[i]+yOffset, 0);
+            
+           // card.transform.position = new Vector3(card.transform.position.x, cardPosY[i], card.transform.position.z);
             card.transform.Rotate(0, 0, cardRotZ[i], Space.Self);
         }
 
