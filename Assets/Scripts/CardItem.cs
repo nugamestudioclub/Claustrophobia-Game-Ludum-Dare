55aebeadce1782f2dac7833fda4c23c6d7cf850c
MarcoHampel <sebastianhampel1@gmail.com>
2023-09-30 12:29:56 -0400

    Added Scriptable Cards

---
+++ Assets/Scripts/CardItem.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCard", menuName = "Cards/Create New Card", order = 1)]
+public class CardItem : ScriptableObject
+{
+    [SerializeField]
+    private Sprite sprite;
+    [SerializeField]
+    private string title;
+    [SerializeField]
+    private string description;
+
+    public Sprite Sprite { get { return sprite; }  }
+    public string Title { get { return title; } }
+    public string Description { get { return description; } }
+}
--- Assets/Scripts/StateMachine/IntroManager.cs
+++ Assets/Scripts/StateMachine/IntroManager.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class IntroManager : IStateManager
 {
-
+    
     // Start is called before the first frame update
     void Start()
     {
