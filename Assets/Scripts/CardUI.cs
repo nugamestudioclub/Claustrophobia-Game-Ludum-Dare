26ebf341809b66b8a2f64b2580eb3e05f7e1b697
Vi <haggertyv@icloud.com>
2023-09-30 12:58:30 -0400

    Card stuff

--- Assets/Scripts/CardUI.cs
+++ Assets/Scripts/CardUI.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
+    private Canvas canvas;
+
+    [SerializeField]
     GameObject cardPrefab;
-    //Image cardImage;
-    string cardText;
+
+    List<int> cardPosX = new List<int> { -200, -100, 0, 100, 200 };
+    List<int> cardPosY = new List<int> { 170, 200, 210, 200, 170 };
+    List<int> cardRotZ = new List<int> { 20, 10, 0, -10, -20 };
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        canvas = GameObject.FindObjectOfType<Canvas>();
+        CardRenderer();
     }
 
     // Update is called once per frame
@@ -23,6 +30,12 @@ public class Card : MonoBehaviour
     // Renders the card in the UI
     void CardRenderer()
     {
-        //Instantiate(this.cardPrefab, new Vector3 (transform.position.x + 1)
+        for (var i = 0; i < 5; i++)
+        {
+            GameObject card = Instantiate(cardPrefab, canvas.transform);
+            card.transform.localPosition = new Vector3(cardPosX[i], cardPosY[i], 0);
+            card.transform.Rotate(0, 0, cardRotZ[i], Space.Self);
+        }
+
     }
 }
