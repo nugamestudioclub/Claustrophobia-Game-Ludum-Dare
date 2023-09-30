65d9fc60f4ac9644960ea38b324330c4142ff277
Vi <haggertyv@icloud.com>
2023-09-30 17:00:22 -0400

    THE BAR WORKS!!!!

--- Assets/Scripts/BarControl.cs
+++ Assets/Scripts/BarControl.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BarControl : MonoBehaviour
 {
+    [SerializeField]
+    private float points;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.GetComponent<Scrollbar>().size = points;
     }
+
 }
