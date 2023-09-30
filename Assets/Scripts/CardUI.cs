7cac11e45b7b983231d81fb5eabb12ac30333127
Vi <haggertyv@icloud.com>
2023-09-30 12:08:53 -0400

    Working on card UI

---
+++ Assets/Scripts/CardUI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    GameObject cardPrefab;
+    //Image cardImage;
+    string cardText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Renders the card in the UI
+    void CardRenderer()
+    {
+        //Instantiate(this.cardPrefab, new Vector3 (transform.position.x + 1)
+    }
+}
