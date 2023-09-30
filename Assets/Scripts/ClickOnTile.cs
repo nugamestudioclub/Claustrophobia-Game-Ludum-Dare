5f31c0193222aa318cd162b33d3541327f147dbb
Vi <haggertyv@icloud.com>
2023-09-30 14:32:15 -0400

    Attempting to scale (help)

--- Assets/Scripts/ClickOnTile.cs
+++ Assets/Scripts/ClickOnTile.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class ClickOnTile : MonoBehaviour
 {
+    [SerializeField] 
+    private float gridIncrement = 2;
+    [SerializeField]
+    private Vector3 gridOffset = Vector3.zero;
+    [SerializeField]
+    private Material gridMaterial;
+
+    [SerializeField]
+    GameObject cube;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gridMaterial.SetVector("_Tiling", Vector2.one * gridIncrement / 2);
+        gridMaterial.SetVector("_Offset", gridOffset / 20);
     }
 
     // Update is called once per frame
@@ -18,10 +29,20 @@ public class ClickOnTile : MonoBehaviour
 
     void FixedUpdate()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, 100))
-            Debug.Log("Hit something");
+            if (Physics.Raycast(ray, out hit, 100) && (hit.collider.gameObject.tag == "TileMap")) {
+                Vector3 hitPoint = hit.point;
+                hitPoint /= gridIncrement;
+                Vector3 rounded = Vector3Int.RoundToInt(hitPoint);
+                rounded *= gridIncrement;
+                rounded += gridOffset;
+                print(rounded);
+                cube.transform.position = new Vector3(rounded.x, 1.002f, rounded.z);
+            }
+        }
     }
 }
